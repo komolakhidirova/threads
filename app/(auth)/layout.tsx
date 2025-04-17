6b783ab32cf8ca26dce0a3fe883e0022c8819c66
Komola Khidirova <komolakhidirova@gmail.com>
2025-04-17 12:40:06 +0500

    #

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,6 @@
 import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from 'next/font/google'
 
-import '../globals.css'
-
 export const metadata = {
 	title: 'Threads',
 	description: 'A Next.js 13 Meta Threads Application',
