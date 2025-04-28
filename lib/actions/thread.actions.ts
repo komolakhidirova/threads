885cb50548340e0b39f604b33e82aecbbf165712
Komola Khidirova <komolakhidirova@gmail.com>
2025-04-28 16:06:55 +0500

    #

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -37,3 +37,34 @@ export async function createThread({
 		throw new Error(`Error creating thread ${error.message}`)
 	}
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+	connectToDB()
+
+	const skipAmount = (pageNumber - 1) * pageSize
+
+	// fetch top level
+	const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+		.sort({ createdAt: 'descending' })
+		.skip(skipAmount)
+		.limit(pageSize)
+		.populate({ path: 'author', model: User })
+		.populate({
+			path: 'children',
+			populate: {
+				path: 'author',
+				model: User,
+				select: '_id name parentId image',
+			},
+		})
+
+	const totalPostsCount = await Thread.countDocuments({
+		parentId: { $in: [null, undefined] },
+	})
+
+	const posts = await postsQuery.exec()
+
+	const isNext = totalPostsCount > skipAmount + posts.length
+
+	return { posts, isNext }
+}
