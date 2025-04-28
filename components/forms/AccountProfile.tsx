35c5592f124a4d3809dd8ea842ed757d559b6220
Komola Khidirova <komolakhidirova@gmail.com>
2025-04-28 12:44:50 +0500

    d

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -7,6 +7,7 @@ import {
 	FormField,
 	FormItem,
 	FormLabel,
+	FormMessage,
 } from '@/components/ui/form'
 import { Input } from '@/components/ui/input'
 import { updateUser } from '@/lib/actions/user.actions'
@@ -142,6 +143,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 									onChange={e => handleImage(e, field.onChange)}
 								/>
 							</FormControl>
+							<FormMessage />
 						</FormItem>
 					)}
 				/>
@@ -161,6 +163,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 									{...field}
 								/>
 							</FormControl>
+							<FormMessage />
 						</FormItem>
 					)}
 				/>
@@ -180,6 +183,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 									{...field}
 								/>
 							</FormControl>
+							<FormMessage />
 						</FormItem>
 					)}
 				/>
@@ -199,6 +203,7 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
 									{...field}
 								/>
 							</FormControl>
+							<FormMessage />
 						</FormItem>
 					)}
 				/>
